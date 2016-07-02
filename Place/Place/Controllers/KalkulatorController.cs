c1a7df8753e89e52fc70620b5b3e1bf67be0b5a5
Pio114 <piotr@parzych.net>
2016-07-02 12:02:46 +0200

    kalkulator +

---
+++ Place/Place/Controllers/KalkulatorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Place.Controllers
+{
+    public class KalkulatorController : Controller
+    {
+        // GET: kalkulator
+        public ActionResult Dodaj(int a, int b)
+        {
+            int suma = a + b;
+            return View("Wynik",suma);
+        }
+    }
+}
