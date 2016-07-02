ddb4a88bfa2fdb3499f3cf1e28efc93e85a48915
Pio114 <piotr@parzych.net>
2016-07-02 13:10:14 +0200

    kalkulator na double

--- Place/Place/Controllers/KalkulatorController.cs
+++ Place/Place/Controllers/KalkulatorController.cs
@@ -8,11 +8,35 @@ namespace Place.Controllers
 {
     public class KalkulatorController : Controller
     {
+        public ActionResult StronaGlownaKalkulatora()
+        {
+            return View();
+        }
+
         // GET: kalkulator
         public ActionResult Dodaj(int a, int b)
         {
             int suma = a + b;
             return View("Wynik",suma);
         }
+
+        public ActionResult Odejmij(int a, int b)
+        {
+            int suma = a - b;
+            return View("Wynik", suma);
+        }
+
+        public ActionResult Mnoz(int a, int b)
+        {
+            int suma = a * b;
+            return View("Wynik", suma);
+        }
+
+        public ActionResult Dziel(double a, double b)
+        {
+            double suma = a / b;
+            return View("Wynik", suma);
+        }
+
     }
 }
